91c2d66c876d28f45449741c35b51e1aa854bc4b
Yuliia Cherkashyna <jul11091109@gmail.com>
2023-06-19 03:27:33 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'modern-normalize';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tweets-test">
       <App />
     </BrowserRouter>
   </React.StrictMode>
